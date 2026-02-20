1b4f66baaaba815ba0f466f5bcac6272d4be1785
Daniel Klecha <daniel.klecha@gmail.com>
2026-02-20 21:22:15 +0100

    add tests for profiles

---
+++ SharpIppTests/Mapping/Profiles/SendUriProfileTest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpIpp.Mapping;
+using SharpIpp.Mapping.Extensions;
+using SharpIpp.Mapping.Profiles;
+using SharpIpp.Models.Requests;
+using SharpIpp.Protocol.Models;
+
+namespace SharpIpp.Tests.Mapping.Profiles;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class SendUriProfileTest
+{
+    [TestMethod]
+    public void Map_SendUriRequestToIppRequestMessage_DocumentUriNullAndLastDocumentFalse_ThrowsArgumentException()
+    {
+        // Arrange
+        var mapper = new SimpleMapper();
+        var assembly = Assembly.GetAssembly(typeof(TypesProfile));
+        mapper.FillFromAssembly(assembly!);
+
+        var request = new SendUriRequest
+        {
+            OperationAttributes = new SendUriOperationAttributes
+            {
+                DocumentUri = null!,
+                LastDocument = false
+            }
+        };
+
+        // Act
+        Action act = () => mapper.Map<SendUriRequest, IppRequestMessage>(request);
+
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage($"{nameof(SendUriOperationAttributes.DocumentUri)} must be set for non-last document");
+    }
+}
