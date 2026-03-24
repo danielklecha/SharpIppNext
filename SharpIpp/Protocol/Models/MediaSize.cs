7d358dde1b3cae701018b660b9cd374d0ec065a1
Paweł Jarosz <paweljarosz3691@gmail.com>
2026-03-24 13:37:57 +0100

    add new [XY]DimensionRange properties. PWG 5100.7 section 6.9.36.1 specifies that: x-dimension can be (integer(1:MAX) or rangeOfInteger(1:MAX))" and y-dimension can be (integer(1:MAX) or rangeOfInteger(1:MAX))"

--- SharpIpp/Protocol/Models/MediaSize.cs
+++ SharpIpp/Protocol/Models/MediaSize.cs
@@ -10,4 +10,14 @@ public class MediaSize
     /// integer(0:MAX))
     /// </summary>
     public int? YDimension { get; set; }
+    
+    /// <summary>
+    /// Range(0:MAX, 0:MAX))
+    /// </summary>
+    public Range? XDimensionRange { get; set; }
+
+    /// <summary>
+    /// Range(0:MAX, 0:MAX))
+    /// </summary>
+    public Range? YDimensionRange { get; set; }
 }
